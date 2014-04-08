c1c351fc84e3d9dcb444ef3108c0f6d2b2257e02
Sindre Moen <sindrenm@gmail.com>
2014-04-08 14:17:17 +0200

    Respond to both HTML and JSON

--- app/controllers/feeder/feeds_controller.rb
+++ app/controllers/feeder/feeds_controller.rb
@@ -2,6 +2,8 @@ require_dependency "feeder/application_controller"
 
 module Feeder
   class FeedsController < ApplicationController
+    respond_to :html, :json
+
     def index
       @items = Item.order(sticky: :desc)
 
@@ -11,6 +13,8 @@ module Feeder
 
       @items = @items.page(params[:page] || 1)
       @items = @items.per(params[:limit] || 25)
+
+      respond_with @items
     end
   end
 end
