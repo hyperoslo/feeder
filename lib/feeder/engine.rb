673b80c18926487d35fcbcc2be2564bd1c959e7b
Sindre Moen <sindrenm@gmail.com>
2014-04-10 00:11:00 +0200

    Expose factories to main app

--- lib/feeder/engine.rb
+++ lib/feeder/engine.rb
@@ -3,5 +3,9 @@ require 'rails-observers'
 module Feeder
   class Engine < ::Rails::Engine
     isolate_namespace Feeder
+
+    initializer 'tasks.factories', after: 'factory_girl.set_factory_paths' do
+      FactoryGirl.definition_file_paths << File.expand_path('../../../spec/factories', __FILE__) if defined?(FactoryGirl)
+    end
   end
 end
