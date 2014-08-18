d90f9b2013dabfc60032e10a418032d276256e36
Sergeev Peter <sergeev.peter@gmail.com>
2014-08-18 13:08:26 +0200

    Feed filter by scopes

--- spec/dummy/app/models/message.rb
+++ spec/dummy/app/models/message.rb
@@ -5,4 +5,5 @@ class Message < ActiveRecord::Base
 
   scope :test_even, -> { where("messages.id % 2 = 0") }
   scope :test_odd, -> { where("messages.id % 2 = 1") }
+  scope :test_gte, -> { where("messages.id >= 3") }
 end
