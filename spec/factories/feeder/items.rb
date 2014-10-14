049dc7e9cf140f894e73f163b5a7cce596aecf55
Espen Høgbakk <espen@hogbakk.no>
2014-10-14 12:51:42 +0200

    Add trait for recommended feed items

--- spec/factories/feeder/items.rb
+++ spec/factories/feeder/items.rb
@@ -3,8 +3,9 @@ FactoryGirl.define do
   factory :feeder_item, class: 'Feeder::Item' do
     feedable nil
 
-    trait(:published) { published_at 1.day.ago }
-    trait(:sticky)    { sticky true }
+    trait(:published)   { published_at 1.day.ago }
+    trait(:sticky)      { sticky true }
+    trait(:recommended) { recommended true }
   end
 
 end
