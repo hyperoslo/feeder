b4f182c1107467d475dd707c35dc0585edf4dd1a
Espen Høgbakk <espen@hogbakk.no>
2014-10-16 14:23:35 +0200

    Add some helper methods

--- app/helpers/feeder/like_helper.rb
+++ app/helpers/feeder/like_helper.rb
@@ -3,5 +3,13 @@ module Feeder
     def valid_like_scope? scope
       Feeder.config.like_scopes.include? scope.to_sym
     end
+
+    def like_scopes
+      Feeder.config.like_scopes
+    end
+
+    def liker
+      send(Feeder.config.current_user_method)
+    end
   end
 end
