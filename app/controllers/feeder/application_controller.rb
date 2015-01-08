91cf39161c90d3e10f42125df70a581eb7a30274
Johannes Gorset <jgorset@gmail.com>
2015-01-08 13:43:37 +0100

    Depend on responders for Rails 4.2 compatibility

--- app/controllers/feeder/application_controller.rb
+++ app/controllers/feeder/application_controller.rb
@@ -1,4 +1,12 @@
+require "responders"
+
+class AppResponder < ActionController::Responder
+  include Responders::FlashResponder
+  include Responders::HttpCacheResponder
+end
+
 module Feeder
   class ApplicationController < ::ApplicationController
+    self.responder = AppResponder
   end
 end
